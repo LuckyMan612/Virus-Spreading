2abc4189d70a84a3efc651429cf2bd8cc03df115
LuckyMan612 <75744132+LuckyMan612@users.noreply.github.com>
2023-07-08 17:20:26 +0200

    Zmieniono scene po kolizji z graczem

--- Assets/! Skrypty/PlayerCollision.cs
+++ Assets/! Skrypty/PlayerCollision.cs
@@ -20,7 +20,7 @@ public class PlayerCollision : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Strzala"))
         {
-            SceneManager.LoadScene(5);
+            SceneManager.LoadScene(1);
         }
     }
 }
