866f8f9a0d06061cfed715590d5446e0ec5b4ddc
LuckyMan612 <75744132+LuckyMan612@users.noreply.github.com>
2023-07-08 15:01:03 +0200

    Dodano wiadomość co się wykrada w zapisaniu do chmury

--- Assets/! Skrypty/chmuraZapisywanie.cs
+++ Assets/! Skrypty/chmuraZapisywanie.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class chmuraZapisywanie : MonoBehaviour
 {
     public GameObject komputer;
     public GameObject loading;
+    public GameObject slider;
+    public TextMeshProUGUI tekst;
+    public List<string> teksty;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +24,19 @@ public class chmuraZapisywanie : MonoBehaviour
     }
     public void Rozpocznij()
     {
-
+        komputer.SetActive(false);
+        loading.SetActive(true);
+        StartCoroutine(Wystartowano());
+    }
+    IEnumerator Wystartowano()
+    {
+        foreach (string t in teksty)
+        {
+            tekst.text = t;
+            slider.GetComponent<Animator>().Play("SliderAnim");
+            yield return new WaitForSeconds(3.5f);
+        }
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(6);
     }
 }
