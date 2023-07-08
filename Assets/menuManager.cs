d3b4fe8f04132b94dbc51b89226da80d84925b83
LuckyMan612 <75744132+LuckyMan612@users.noreply.github.com>
2023-07-08 21:53:20 +0200

    CursorUnlock

---
+++ Assets/menuManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuManager : MonoBehaviour
+{
+    public GameObject nick;
+    public GameObject graj;
+    public bool oszukaj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.GetString("nick", "null") == "null" && !oszukaj)
+        {
+            Debug.Log(PlayerPrefs.GetString("nick", "null"));
+            nick.SetActive(true);
+            graj.SetActive(false);
+        }
+        Debug.Log(PlayerPrefs.GetString("nick", "null"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
