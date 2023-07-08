98ac13a82f4a9feea36f22ab8a80a406c3038d14
LuckyMan612 <75744132+LuckyMan612@users.noreply.github.com>
2023-07-08 18:46:17 +0200

    Dodano centrum hackera

---
+++ Assets/! Skrypty/generowanieKomputerow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generowanieKomputerow : MonoBehaviour
+{
+    /*public GameObject jakWygladaKomputer;
+    public Vector3 ostatniaPozycja;*/
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*for (int i = 0; i < Random.Range(20, 40); i++)
+        {
+            GameObject powstaly = Instantiate(jakWygladaKomputer);
+            powstaly.GetComponent<RectTransform>().position = new Vector3(ostatniaPozycja.x, ostatniaPozycja.y - 217, ostatniaPozycja.z);
+            powstaly.transform.SetParent(this.gameObject.transform, true);
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
